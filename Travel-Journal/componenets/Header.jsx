9a020eb51e238554e8c177db56a379e0a88d821a
fazza-tech <ebranfazza@gmail.com>
2024-12-22 10:11:50 +0530

    commited all

---
+++ Travel-Journal/componenets/Header.jsx
@@ -0,0 +1,10 @@
+export default function(){
+    return(
+        <>
+            <header className="header">
+                    <img src="/globe.png" alt="globe-image" />
+                    <h1>my travel journal.</h1>
+            </header>
+        </>
+    )
+}
---
+++ Travel-Journal/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "../componenets/Header"
+import Entry from "../componenets/Entry"
+
+export default function App(){
+  return(
+    <>
+    <Header/>
+    <Entry/>
+    <Entry/>
+    <Entry/>
+    </>
+    
+  )
+}
